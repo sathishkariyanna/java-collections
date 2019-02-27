c07cec01fd5ae375aa89db2da9bd461a21a9ad94
Sathish Kariyanna <38923455+sathishkariyanna@users.noreply.github.com>
2019-02-27 15:02:01 -0600

    adding condition to check duplicate element while adding to set

--- src/com/sathish/collections/set/HashSetExample.java
+++ src/com/sathish/collections/set/HashSetExample.java
@@ -18,15 +18,19 @@ public class HashSetExample {
 		hash_SetA.add("Sathish");
 		hash_SetA.add("Tom");
 		hash_SetA.add("King");
-		hash_SetA.add("Mike");
-		hash_SetA.add("Tom");
-		System.out.print("Set output without the duplicates");
-		System.out.println(hash_SetA);
+		if(hash_SetA.add("Mike"))
+			System.out.println("unique element added");
+		if(hash_SetA.add("Tom")) {
+			System.out.println("duplicate element overwritten"); 
+			//in HashSet it will not over write, it will check the key and skips if it is present			
+		}else {
+			System.out.println("duplicate element did not add");
+		}
+		System.out.println("Hash set : " + hash_SetA);
 
 		// Set demonstration using TreeSet (Converting HashSet to TreeSet)
-		System.out.print("Sorted Set after passing into TreeSet");
 		Set<String> tree_Set = new TreeSet<String>(hash_SetA);
-		System.out.println(tree_Set);
+		System.out.println("Sorted Set after passing into TreeSet : " + tree_Set);
 
 		Set<String> hash_SetB = new HashSet<String>();
 		hash_SetB.add("Jon");
@@ -37,40 +41,37 @@ public class HashSetExample {
 		// To find union
 		Set<String> union = new HashSet<String>(hash_SetA);
 		union.addAll(hash_SetB);
-		System.out.print("Union of the two Set");
-		System.out.println(union);
+		System.out.println("Union of the two Set : " + union);
 
 		// To find intersection
 		Set<String> intersection = new HashSet<String>(hash_SetA);
 		intersection.retainAll(hash_SetB);
-		System.out.print("Intersection of the two Set");
-		System.out.println(intersection);
+		System.out.println("Intersection of the two Set : " + intersection);
 
 		// To find the symmetric difference
 		Set<String> difference = new HashSet<String>(hash_SetA);
 		difference.removeAll(hash_SetB); // something like A-B
-		System.out.print("Difference of the two Set");
-		System.out.println(difference);
+		System.out.println("Difference of the two Set : " + difference);
 
 		// Using isEmpty() to verify for the emptiness
 		System.out.println("The set is empty? " + hash_SetA.isEmpty());
 
 		// Does the set contain "Sathish"
-		System.out.println("Does the set contain 'Sathish'?" + hash_SetA.contains("Sathish"));
+		System.out.println("Does the set contain 'Sathish'? " + hash_SetA.contains("Sathish"));
 
 		// Getting the size of the set
-		System.out.println("The size of the set is " + hash_SetA.size());
+		System.out.println("The size of the set is : " + hash_SetA.size());
 
 		// Removing "Tom" from the set
 		hash_SetA.remove("Tom");
 
 		// Displaying the HashSet
-		System.out.println("HashSet: " + hash_SetA);
+		System.out.println("HashSet : " + hash_SetA);
 
 		// to iterate set we can use Iterator interface or any other method.
 		Iterator iterator = hash_SetA.iterator();
 		// displaying the set data using Iterator
-		System.out.println("Iterating Set data: ");
+		System.out.println("Iterating Set data : ");
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next() + " ");
 		}
