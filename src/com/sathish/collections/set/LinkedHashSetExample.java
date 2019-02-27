3ca950267033b90c22afd660af04b448aa2a3f9b
Sathish Kariyanna <38923455+sathishkariyanna@users.noreply.github.com>
2019-02-27 15:03:35 -0600

    adding condition to check duplicate element while adding to set

--- src/com/sathish/collections/set/LinkedHashSetExample.java
+++ src/com/sathish/collections/set/LinkedHashSetExample.java
@@ -1,6 +1,9 @@
 package com.sathish.collections.set;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * 
@@ -11,7 +14,7 @@ public class LinkedHashSetExample {
 
 	public static void main(String args[]) {
 		// LinkedHashSet of String Type
-		LinkedHashSet<String> lhset = new LinkedHashSet<String>();
+		Set<String> lhset = new LinkedHashSet<String>();
 
 		// Adding elements to the LinkedHashSet
 		lhset.add("Y");
@@ -22,16 +25,30 @@ public class LinkedHashSetExample {
 		System.out.println("LinkedHashSet of String : " + lhset);
 
 		// This will not add new element as N already exists
-		lhset.add("N");
+		if(lhset.add("N"))
+			System.out.println("duplicate element overwritten");
+		else {
+			System.out.println("duplicate element did not add");
+			//in Set it will not over write		
+		}
 		lhset.add("E");
 
 		System.out.println("Size of LinkedHashSet = " + lhset.size());
 		System.out.println("Original LinkedHashSet:" + lhset);
 		System.out.println("Removing K from LinkedHashSet: " + lhset.remove("K"));
-		System.out.println("Trying to Remove C which is not " + "present: " + lhset.remove("C"));
+		System.out.println("Trying to Remove C which is not " + "present: " + lhset.remove("C")); //this will not give any exception
 		System.out.println("Checking if N is present=" + lhset.contains("N"));
 		System.out.println("Updated LinkedHashSet: " + lhset);
 
+		//iterate linkedHashSet using Iterator
+		Iterator<String> setIterator = lhset.iterator();
+		while(setIterator.hasNext()){
+			System.out.println(setIterator.next());
+		}
+		//Convert LinkedHashSet of String objects to TreeSet collection 
+		Set<String> linkedhashSetToTreeSet = new TreeSet<>(lhset);
+		System.out.println("Converted TreeSet : "+linkedhashSetToTreeSet);	
+		
 		// LinkedHashSet of Integer Type
 		LinkedHashSet<Integer> lhset2 = new LinkedHashSet<Integer>();
 
